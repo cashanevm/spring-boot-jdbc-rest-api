785222c425212dc2ce80093d860d00b77195be5f
Oleksandr Nevmerzhytskyi <oleksandr.nevmerzhytskyi@extreme-idea.com>
2022-02-23 18:01:28 +0200

    init

---
+++ src/test/java/com/jdbc/rest/SpringBootJdbcRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbc.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
